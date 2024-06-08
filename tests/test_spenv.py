9299a6fa096c6193d7d06833fde4695a14b5489f
jotasaraiva <jpsaraiva2002@gmail.com>
2024-06-08 20:12:33 -0300

    Adds tests

--- tests/test_spenv.py
+++ tests/test_spenv.py
@@ -1,1 +1,17 @@
-from spenv import spenv
+from spenv import mvspec, specenv
+import pandas as pd
+import numpy as np
+
+def test_mvspec():
+    """Test multivariate spectral estimation."""
+    data = pd.read_csv("tests/nyse.csv")
+    assert mvspec(data.value)['freq'].size == 1000, "Frequency estimation is incorrect"
+
+def test_specenv():
+    """Test spectral envelope."""
+    data = pd.read_csv("tests/nyse.csv")["value"].values.reshape([-1, 1])
+    xdata = np.concatenate([data, np.abs(data), data**2], axis=1)
+    res = specenv(xdata).shape[0]
+    assert res == 1000, "Spectral envelope frequencies are different sizes"
+
+
